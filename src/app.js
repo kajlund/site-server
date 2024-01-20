245e77731607a2ccd1d73342b88672dc38bf3b63
Kaj Lund <kaj.lund@gmail.com>
2024-01-20 22:08:18 +0200

    Adds cors

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from 'express'
 import path from 'path'
 
@@ -7,10 +8,13 @@ const app = express()
 app.use(express.json())
 // Middleware for url encoding
 app.use(express.urlencoded({ extended: true }))
+// Middleware for handling CORS policy
+app.use(cors())
 
 // Serve static assets
 app.use(express.static(path.join(process.cwd(), 'public')))
 
+// Helper endpoints for service check
 app.get('/ping', (req, res) => {
   res.send('pong')
 })
