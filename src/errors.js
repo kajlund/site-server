950f749cc1da33126a072e18e8ca8e1904f3b2f2
Kaj Lund <kaj.lund@gmail.com>
2024-01-20 22:25:46 +0200

    Adds custom errors plus status codes and messages

---
+++ src/errors.js
@@ -0,0 +1,37 @@
+import { codes, phrases } from './statuscodes.js'
+
+export class AppError extends Error {
+  constructor(message = phrases.INTERNAL_SERVER_ERROR, code = codes.INTERNAL_SERVER_ERROR, detail = '') {
+    super(message)
+    this.name = this.constructor.name
+    this.isAppError = true
+    this.statusCode = code
+    this.detail = detail
+    Error.captureStackTrace(this, this.constructor)
+  }
+}
+
+export class UnautorizedError extends AppError {
+  constructor(detail = '') {
+    super(phrases.UNAUTHORIZED, codes.UNAUTHORIZED, detail)
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(detail = '', errors = {}) {
+    super(phrases.BAD_REQUEST, codes.BAD_REQUEST, detail)
+    this.errors = errors
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(detail = '') {
+    super(phrases.NOT_FOUND, codes.NOT_FOUND, detail)
+  }
+}
+
+export class InternalServerError extends AppError {
+  constructor(detail = '') {
+    super(phrases.INTERNAL_SERVER_ERROR, codes.INTERNAL_SERVER_ERROR, detail)
+  }
+}
---
+++ src/statuscodes.js
@@ -0,0 +1,21 @@
+export const codes = {
+  OK: 200,
+  CREATED: 201,
+  NO_CONTENT: 204,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  NOT_IMPLEMENTED: 501,
+}
+
+export const phrases = {
+  OK: 'OK',
+  CREATED: 'Created',
+  NO_CONTENT: 'No content',
+  BAD_REQUEST: 'Bad Request',
+  UNAUTHORIZED: 'Unauthorized',
+  NOT_FOUND: 'Not Found',
+  INTERNAL_SERVER_ERROR: 'Internal Server Error',
+  NOT_IMPLEMENTED: 'Not Implemented',
+}
