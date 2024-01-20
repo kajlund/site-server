2dc93661a811df0b989cd73effd2f48c8fe48264
Kaj Lund <kaj.lund@gmail.com>
2024-01-20 22:26:15 +0200

    Adds 404 handler

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import cors from 'cors'
 import express from 'express'
 import path from 'path'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
+import notFoundHandler from './middleware/notfound.handler.js'
+
 const app = express()
 
 // Middleware to handle incoming JSON payloads
@@ -18,5 +23,8 @@ app.use(express.static(path.join(process.cwd(), 'public')))
 app.get('/ping', (req, res) => {
   res.send('pong')
 })
+
+// 404 Handler
+app.use(notFoundHandler)
 
 export default app
