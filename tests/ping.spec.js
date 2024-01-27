2c0083744e1139e8f8af83f69a3a59f939c921e9
Kaj Lund <kaj.lund@gmail.com>
2024-01-27 23:21:27 +0200

    Removed Japa in favor of testing using Bruno

--- tests/ping.spec.js
+++
@@ -1,7 +0,0 @@
-import { test } from '@japa/runner'
-
-test('get /ping', async ({ client, assert }) => {
-  const response = await client.get('/ping')
-  // response.dump()
-  assert.equal(response.text(), 'pong')
-})
