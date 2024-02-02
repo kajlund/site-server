4c599b657e9db19c22d5667d3e762f5c9e15c836
Kaj Lund <kaj.lund@gmail.com>
2024-02-02 23:56:41 +0200

    Fixed user seeder pwd

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -1,4 +1,5 @@
 import crypto from 'crypto'
+import bcrypt from 'bcryptjs'
 
 /**
  * @param { import("knex").Knex } knex
@@ -7,12 +8,15 @@ import crypto from 'crypto'
 export async function seed(knex) {
   // Deletes existing entries
   await knex('users').del()
+  const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS, 10)
+  const salt = await bcrypt.genSalt(SALT_ROUNDS)
+  const password = await bcrypt.hash(process.env.ADMIN_SEED_PWD, salt)
   await knex('users').insert([
     {
       id: crypto.randomUUID(),
       email: 'kaj.lund@gmail.com',
       alias: 'luka',
-      password: process.env.ADMIN_SEED_PWD,
+      password,
       role: 'admin',
     },
   ])
